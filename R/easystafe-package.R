89d74cc04d71f7710b51663a2f29de83293f99da
Joe Lara <joseph.lara@gmail.com>
2025-11-24 19:13:52 +0200

    *feat* add badges

--- R/easystafe-package.R
+++ R/easystafe-package.R
@@ -1,7 +1,20 @@
+#' easystafe: Ferramentas para Processamento de Extractos do e-SISTAFE
+#'
+#' O pacote **easystafe** fornece funções para processamento, deduplicação,
+#' extração de metadados e harmonização de extractos orçamentais provenientes
+#' do sistema e-SISTAFE. Inclui um pipeline robusto para preparação de dados
+#' para análise financeira, monitoria de execução e integração com workflows
+#' baseados em R.
+#'
+#' Este pacote encontra-se atualmente em fase **experimental**.
+#'
+#' @docType package
+#' @name easystafe
 #' @keywords internal
+#'
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom lifecycle experimental
 ## usethis namespace: end
+
 NULL
